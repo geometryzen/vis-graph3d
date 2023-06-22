d04abc7133b06837ee6a1b6ca778958842c4cddb
David Geo Holmes <geometryzen@gmail.com>
2023-06-22 14:08:33 -0400

    Initial Commit

--- tests/DataSet.spec.ts
+++ tests/DataSet.spec.ts
@@ -1,8 +1,9 @@
 import { createDataSet, DataSet } from "../src/index";
-
+/*
 function custom(x: number, y: number, t: number): number {
     return Math.sin(x / 50 - t / 10) * Math.cos(y / 50 + t / 10) * 50 + 50;
 }
+*/
 
 test("DataSet", function () {
     const data = createDataSet();
@@ -18,8 +19,8 @@ test("DataSet.add", function () {
     for (let t = 0; t < tMax; t++) {
         for (let x = 0; x < axisMax; x += axisStep) {
             for (let y = 0; y < axisMax; y += axisStep) {
-                const value = custom(x, y, t);
-                data.add([{ x: x, y: y, z: value, filter: t, style: value }]);
+                // const value = custom(x, y, t);
+                // data.add([{ x: x, y: y, z: value, filter: t, style: value }]);
             }
         }
     }
